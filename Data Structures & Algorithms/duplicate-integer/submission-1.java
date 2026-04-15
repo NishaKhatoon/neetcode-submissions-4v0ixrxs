b54ff375f0840268a14626b0565b904b5ecfd64f
Nisha Khatoon <786ariba7233@gmail.com>
2026-04-15 13:05:46 +0530

    Add: duplicate-integer - submission-1

---
+++ Data Structures & Algorithms/duplicate-integer/submission-1.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+
+       int len=nums.length;
+        Arrays.sort(nums);
+       for(int i=1; i<len; i++){
+        if(nums[i]==nums[i-1]) return true;
+
+       } 
+
+       return false;
+    }
+}
